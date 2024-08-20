2b1cbb2f132db6a0b8bee914b9e379c2ea7db2c7
AmanBhaskar16 <141729237+AmanBhaskar16@users.noreply.github.com>
2024-08-20 12:08:12 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,16 @@
+# Basic arithmetic operations in python
+
+a = int(input("Enter 1st number : "));
+b = int(input("Enter 2nd number : "));
+print("The sum is : ");
+print(a+b);
+print("The product is : ");
+print(a*b);
+print("The fraction quotient is : ");
+print(a/b);
+print("The subtraction is : ");
+print(a-b);
+print("The integer quotient is : ");
+print(a//b);
+print("The remainder is : ");
+print(a%b);
